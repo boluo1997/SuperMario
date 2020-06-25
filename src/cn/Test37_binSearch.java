45d26cdcd98e5ab2358cb67203f988fe41778bc0
dingchao <1109928059@qq.com>
2020-06-25 22:03:41 +0800

    二分查找非递归写法

--- src/cn/Test37_binSearch.java
+++ src/cn/Test37_binSearch.java
@@ -7,8 +7,32 @@ public class Test37_binSearch {
         int[] nums = {5,9,13,28,36,51,66,74,89,93,95,97};
 
         int result = binSearch(nums,0,nums.length-1,51);
-
         System.out.println(result);
+
+        //非递归写法
+        int key = 36;
+        int min = 0;
+        int max = nums.length-1;
+        int mid = (min+max)/2;
+        
+        while(key != nums[mid]){
+            if(key > nums[mid]){
+                min = mid+1;
+            }else {
+                max = mid-1;
+            }
+
+            if(min >= key){
+                mid = -1;
+                break;
+            }
+
+            mid = (min+max)/2;
+        }
+
+        System.out.println(mid);
+
+
     }
 
     //递归写法
@@ -24,10 +48,6 @@ public class Test37_binSearch {
         }else {
             return -1;
         }
-
     }
-
-
-
 
 }
